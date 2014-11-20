bfb4747a1a9c37cab0ce1e6416944a411d4fe5ac
hydedio <LinyesMacmini@Linkyes-Mac-mini.local>
2014-11-20 14:29:19 +0800

    test commet

---
+++ GCD_Async_Tst/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCD_Async_Tst
+//
+//  Created by Linyes Mac mini on 2014/10/14.
+//  Copyright (c) 2014年 linkyes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GCD_Async_Tst/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GCD_Async_Tst
+//
+//  Created by Linyes Mac mini on 2014/10/14.
+//  Copyright (c) 2014年 linkyes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
